836b650bd86d2bb3b1245dd4f78627894e2f921b
Jonathan Omarwoth <oma0256@gmail.com>
2018-10-16 14:24:08 +0300

    Test user model

---
+++ tests/test_models.py
@@ -0,0 +1,22 @@
+"""
+File to test the application models
+"""
+
+import unittest
+from api.models import User
+
+
+class TestModels(unittest.TestCase):
+    """
+    Test app models
+    """
+    def test_user_model(self):
+        """
+        Test initializing a User object
+        """
+        user = User(1, "first", "last", "first@email.com", "pass1234")
+        self.assertEqual(user.id, 1)
+        self.assertEqual(user.first_name, "first")
+        self.assertEqual(user.last_name, "last")
+        self.assertEqual(user.email, "first@email.com")
+        self.assertEqual(user.password, "pass1234")
