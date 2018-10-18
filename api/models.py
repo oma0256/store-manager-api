f00d5d6fc20e076e239f5c9d80fa9b4f0a66a179
Jonathan Omarwoth <oma0256@gmail.com>
2018-10-18 23:40:40 +0300

    Change to function based views

--- api/models.py
+++ api/models.py
@@ -17,6 +17,18 @@ class User:
         self.is_admin = is_admin
 
 
+class Product:
+    """
+    Define product structure
+    """
+    def __init__(self, product_id, name, price, quantity, category):
+        self.product_id = product_id
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.category = category
+
+
 class Sale:
     """
     Define sale structure
