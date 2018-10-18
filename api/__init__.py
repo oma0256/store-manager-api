3a161d0f967b1ce100432aaeffbee15781562b80
Jonathan Omarwoth <oma0256@gmail.com>
2018-10-18 15:07:43 +0300

    Clear session whenever server is restarted

--- api/__init__.py
+++ api/__init__.py
@@ -2,9 +2,17 @@
 Set up flask
 """
 
-from flask import Flask
+from flask import Flask, session
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
+
+
+@app.before_first_request
+def clear_session():
+    """
+    Clear session whenever server restarts
+    """
+    session.clear()
 
 from api import views
