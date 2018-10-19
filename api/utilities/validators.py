4d614d2428d1038ffb225ab8cbd183d1ee621e8b
Jonathan Omarwoth <oma0256@gmail.com>
2018-10-19 09:39:21 +0300

    Improve validate product

--- api/utilities/validators.py
+++ api/utilities/validators.py
@@ -10,22 +10,22 @@ def validate_product(name, price, quantity, category):
     Funtion to validate product data
     """
     # Check if fields are empty
-    if not name:
-        return jsonify({"error": "Product name is required"}), 400
-    if not price:
-        return jsonify({"error": "Product price is required"}), 400
-    if not quantity:
-        return jsonify({"error": "Product quantity is required"}), 400
-    if not category:
-        return jsonify({"error": "Product category is required"}), 400
+    if not name or not price or not quantity or not category:
+        return jsonify({"error": "This field is required"}), 400
+    # if not price:
+    #     return jsonify({"error": "Product price is required"}), 400
+    # if not quantity:
+    #     return jsonify({"error": "Product quantity is required"}), 400
+    # if not category:
+    #     return jsonify({"error": "Product category is required"}), 400
 
     # Checks if price and quantity are integers
-    if not isinstance(price, int):
-        return jsonify({
-            "error": "Product price is invalid please an integer"
-            }), 400
-    if not isinstance(quantity, int):
-        return jsonify({
-            "error": "Product quantity is invalid please an integer"
-            }), 400
+    # if not isinstance(price, int):
+    #     return jsonify({
+    #         "error": "Product price is invalid please an integer"
+    #         }), 400
+    # if not isinstance(quantity, int):
+    #     return jsonify({
+    #         "error": "Product quantity is invalid please an integer"
+    #         }), 400
     return None
