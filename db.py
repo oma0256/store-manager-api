c556327201d4363429f2005503350094c60f3d67
Jonathan <oma0256@gmail.com>
2018-10-30 11:40:27 +0300

    Change db scheme

--- db.py
+++ db.py
@@ -56,57 +56,57 @@ class DB:
     
     def create_admin(self):
         """Function to create an admin"""
-        self.cur.execute("INSERT INTO public.users(first_name, last_name, email, password, is_admin) VALUES (%s, %s, %s, %s, %s)",
+        self.cur.execute("INSERT INTO users(first_name, last_name, email, password, is_admin) VALUES (%s, %s, %s, %s, %s)",
                          ("admin", "owner", "admin@email.com", generate_password_hash("pass1234"), True))
     
     def get_user(self, email):
-        self.cur.execute("SELECT * FROM public.users WHERE email=%s", (email,))
+        self.cur.execute("SELECT * FROM users WHERE email=%s", (email,))
         return self.cur.fetchone()
     
     def create_user(self, user):
-        self.cur.execute("INSERT INTO public.users(first_name, last_name, email, password) VALUES (%s, %s, %s, %s)", 
+        self.cur.execute("INSERT INTO users(first_name, last_name, email, password) VALUES (%s, %s, %s, %s)", 
                          (user.first_name, user.last_name, user.email, user.password))
     
     def delete_attendants(self):
-        self.cur.execute("DELETE FROM public.users WHERE is_admin=%s", (False,))
+        self.cur.execute("DELETE FROM users WHERE is_admin=%s", (False,))
     
     def delete_products(self):
-        self.cur.execute("TRUNCATE public.products")
+        self.cur.execute("TRUNCATE products")
     
     def add_product(self, product):
-        self.cur.execute("INSERT INTO public.products(name, unit_cost, quantity) VALUES (%s, %s, %s)", 
+        self.cur.execute("INSERT INTO products(name, unit_cost, quantity) VALUES (%s, %s, %s)", 
                          (product.name, product.unit_cost, product.quantity))
     
     def get_product_by_name(self, name):
-        self.cur.execute("SELECT * FROM public.products WHERE name=%s", (name,))
+        self.cur.execute("SELECT * FROM products WHERE name=%s", (name,))
         return self.cur.fetchone()
     
     def get_products(self):
-        self.cur.execute("SELECT * FROM public.products")
+        self.cur.execute("SELECT * FROM products")
         return self.cur.fetchall()
     
     def get_product_by_id(self, product_id):
-        self.cur.execute("SELECT * FROM public.products WHERE id=%s", (product_id,))
+        self.cur.execute("SELECT * FROM products WHERE id=%s", (product_id,))
         return self.cur.fetchone()
     
     def update_product(self, name, unit_cost, quantity, product_id):
-        self.cur.execute("UPDATE public.products SET name=%s, unit_cost=%s, quantity=%s WHERE id=%s", 
+        self.cur.execute("UPDATE products SET name=%s, unit_cost=%s, quantity=%s WHERE id=%s", 
                          (name, unit_cost, quantity, product_id))
     
     def delete_product(self, product_id):
-        self.cur.execute("DELETE FROM public.products WHERE id=%s", (product_id,))
+        self.cur.execute("DELETE FROM products WHERE id=%s", (product_id,))
 
     def delete_sales(self):
-        self.cur.execute("TRUNCATE public.sales")
+        self.cur.execute("TRUNCATE sales")
 
     def add_sale(self, attendant, products, total):
-        self.cur.execute("INSERT INTO public.sales(attendant, cart_items, total) VALUES (%s, %s, %s)",
+        self.cur.execute("INSERT INTO sales(attendant, cart_items, total) VALUES (%s, %s, %s)",
                      (attendant, products, total))
 
     def get_sale_records(self):
-        self.cur.execute("SELECT * FROM public.sales")
+        self.cur.execute("SELECT * FROM sales")
         return self.cur.fetchall()
 
     def get_single_sale(self, sale_id):
-        self.cur.execute("SELECT * FROM public.sales WHERE id=%s", (sale_id,))
+        self.cur.execute("SELECT * FROM sales WHERE id=%s", (sale_id,))
         return self.cur.fetchone()
