2dc31127d8e4fb0460b9db64b760c59339bf1ac3
Jonathan <oma0256@gmail.com>
2018-10-31 08:00:29 +0300

    Remove duplication

--- api/views.py
+++ api/views.py
@@ -418,22 +418,21 @@ class CategoryView(MethodView):
         loggedin_user = db_conn.get_user(current_user)
         # Check if it's not store owner
         if not loggedin_user["is_admin"]:
-            return jsonify({
-                "error": "Please login as a store owner"
-            }), 403
-        
-        # Check if category exists
-        category = db_conn.get_category_by_id(int(category_id))
-        if not category:
-            return jsonify({
-                "error": "Category you're trying to delete doesn't exist"
-            }), 404
+            # Check if category exists
+            category = db_conn.get_category_by_id(int(category_id))
+            if not category:
+                return jsonify({
+                    "error": "Category you're trying to delete doesn't exist"
+                }), 404
 
-        # Delete category
-        db_conn.delete_category(int(category_id))
+            # Delete category
+            db_conn.delete_category(int(category_id))
+            return jsonify({
+                "message": "Category has been deleted successfully"
+            })
         return jsonify({
-            "message": "Category has been deleted successfully"
-        })
+            "error": "Please login as a store owner"
+        }), 403
 
 
 # Map urls to view classes
